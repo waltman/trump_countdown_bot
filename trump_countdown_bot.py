51132116987adbf12812c6c98e61c50a1850a11a
Walt Mankowski <waltman@pobox.com>
2020-11-08 19:10:15 -0500

    Removed !hello target

--- trump_countdown_bot.py
+++ trump_countdown_bot.py
@@ -42,10 +42,6 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
-
     if message.content == 'countdown':
         msg = countdown()
         print(f'Message from {message.author}, countdown={msg}')
