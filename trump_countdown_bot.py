355d4d973ceae019ec0effe67c63309e7fbd73b3
Walt Mankowski <waltman@pobox.com>
2021-08-24 20:07:22 -0400

    added info about a possible 3rd jab 8 months after the 2nd

--- trump_countdown_bot.py
+++ trump_countdown_bot.py
@@ -53,6 +53,7 @@ vaccine_jab1 = int(time.mktime(time.strptime("2021-04-15 13:30", "%Y-%m-%d %H:%M
 vaccine_jab1_week = int(time.mktime(time.strptime("2021-04-29 13:30", "%Y-%m-%d %H:%M")))
 vaccine_jab2 = int(time.mktime(time.strptime("2021-05-06 13:30", "%Y-%m-%d %H:%M")))
 vaccine_jab2_week = int(time.mktime(time.strptime("2021-05-20 13:30", "%Y-%m-%d %H:%M")))
+vaccine_jab3 = int(time.mktime(time.strptime("2022-01-06 13:30", "%Y-%m-%d %H:%M")))
 
 client = discord.Client()
 
@@ -84,7 +85,8 @@ async def on_message(message):
         m2 = countdown(vaccine_jab1_week)
         m3 = countdown(vaccine_jab2)
         m4 = countdown(vaccine_jab2_week)
-        msg = f'**First jab:** {m1}\n**14 days after first jab:** {m2}\n**Second jab:** {m3}\n**14 days after second jab:** {m4}'
+        m5 = countdown(vaccine_jab3)
+        msg = f'**First jab:** {m1}\n**14 days after first jab:** {m2}\n**Second jab:** {m3}\n**14 days after second jab:** {m4}\n**8 months after second jab:** {m5}'
         print(f'Message from {message.author}, walt vaccine={msg}')
         await message.channel.send(msg)
 
