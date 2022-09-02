3dcde87d763004490026c95f126f45a08fe8f03b
Walt Mankowski <waltman@pobox.com>
2022-09-01 20:57:58 -0400

    updated to work with the new API in discord.py 2.0

--- trump_countdown_bot.py
+++ trump_countdown_bot.py
@@ -87,62 +87,60 @@ vaccine_jab2_week = int(time.mktime(time.strptime("2021-05-20 13:30", "%Y-%m-%d 
 vaccine_jab3 = int(time.mktime(time.strptime("2021-11-11 13:30", "%Y-%m-%d %H:%M")))
 vaccine_jab4 = int(time.mktime(time.strptime("2022-06-17 10:15", "%Y-%m-%d %H:%M")))
 
-client = discord.Client()
+class TrumpCountdownClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as', self.user)
+        print(f'{self.user} connected to Discord!')
+        print('------')
 
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author == self.user:
+            return
 
-    content = message.content.lower()
-    if content == 'countdown':
-        msg = countdown(inaug_epoch_2025)
-        print(f'Message from {message.author}, countdown={msg}')
-        await message.channel.send(msg)
-    elif content == 'swatch':
-        msg = swatch()
-        print(f'Message from {message.author}, swatch={msg}')
-        await message.channel.send(msg)
-    elif content == 'chomsky':
-        msg = chomsky()
-        print(f'Message from {message.author}, chomsky={msg}')
-        await message.channel.send(msg)
-    elif content == 'conspiracy':
-        msg = conspiracy()
-        print(f'Message from {message.author}, conspiracy={msg}')
-        await message.channel.send(msg)
-    elif content == 'well actually':
-        msg = well_actually()
-        print(f'Message from {message.author}, well_actually={msg}')
-        await message.channel.send(msg)
-    elif content == 'walt vaccine':
-        m1 = countdown(vaccine_jab1)
-        m2 = countdown(vaccine_jab1_week)
-        m3 = countdown(vaccine_jab2)
-        m4 = countdown(vaccine_jab2_week)
-        m5 = countdown(vaccine_jab3)
-        m6 = countdown(vaccine_jab4)
-        msg = f'**First jab:** {m1}\n**Second jab:** {m3}\n**First booster:** {m5}\n**Second booster:** {m6}'
-        print(f'Message from {message.author}, walt vaccine={msg}')
-        await message.channel.send(msg)
-    elif content.startswith('covid pa graph'):
-        county = content.split()[-1].title()
-        fname = os.path.join('/home/waltman/perl/projects/covid19/graphs', county + "_cases.png")
-        if os.path.exists(fname):
-            print(f'Message from {message.author}, sending {fname}')
-            await message.channel.send(file=discord.File(fname))
-        else:
-            msg = f'Sorry, no graph for {county}'
-            print(f"Message from {message.author}, {fname} doesn't exist")
+        content = message.content.lower()
+        if content == 'countdown':
+            msg = countdown(inaug_epoch_2025)
+            print(f'Message from {message.author}, countdown={msg}')
             await message.channel.send(msg)
+        elif content == 'swatch':
+            msg = swatch()
+            print(f'Message from {message.author}, swatch={msg}')
+            await message.channel.send(msg)
+        elif content == 'chomsky':
+            msg = chomsky()
+            print(f'Message from {message.author}, chomsky={msg}')
+            await message.channel.send(msg)
+        elif content == 'conspiracy':
+            msg = conspiracy()
+            print(f'Message from {message.author}, conspiracy={msg}')
+            await message.channel.send(msg)
+        elif content == 'well actually':
+            msg = well_actually()
+            print(f'Message from {message.author}, well_actually={msg}')
+            await message.channel.send(msg)
+        elif content == 'walt vaccine':
+            m1 = countdown(vaccine_jab1)
+            m2 = countdown(vaccine_jab1_week)
+            m3 = countdown(vaccine_jab2)
+            m4 = countdown(vaccine_jab2_week)
+            m5 = countdown(vaccine_jab3)
+            m6 = countdown(vaccine_jab4)
+            msg = f'**First jab:** {m1}\n**Second jab:** {m3}\n**First booster:** {m5}\n**Second booster:** {m6}'
+            print(f'Message from {message.author}, walt vaccine={msg}')
+            await message.channel.send(msg)
+        elif content.startswith('covid pa graph'):
+            county = content.split()[-1].title()
+            fname = os.path.join('/home/waltman/perl/projects/covid19/graphs', county + "_cases.png")
+            if os.path.exists(fname):
+                print(f'Message from {message.author}, sending {fname}')
+                await message.channel.send(file=discord.File(fname))
+            else:
+                msg = f'Sorry, no graph for {county}'
+                print(f"Message from {message.author}, {fname} doesn't exist")
+                await message.channel.send(msg)
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print(f'{client.user} connected to Discord!')
-    print('------')
-    
+intents = discord.Intents.default()
+intents.message_content = True
+client = TrumpCountdownClient(intents=intents)
 client.run(TOKEN)
